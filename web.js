47ff2ae1f07b2156931a984e88c92096502e8931
Jeff Pape <jeff.pape@gmail.com>
2013-07-09 01:54:38 +0000

    index.html

--- web.js
+++ web.js
@@ -2,9 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var file_name = 'index.html'
-var buffer = new Buffer(30)
-buffer = fs.readFileSync(file_name)
-var message = buffer.toString()
 var message = 'Howdy!'
 
 app.get('/', function(request, response) {
